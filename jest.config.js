1c91180ba3e4fbb09675f69f0e6afc6105cc72aa
Kyleigh Poultney <kyleigh.poultney@gmail.com>
2024-02-15 11:44:42 -0500

    Shell code suggestion adde

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  clearMocks: true,
-  testMatch: ['**/*test.ts'],
-  testPathIgnorePatterns: ['<rootDir>/[/\\\\]node_modules[/\\\\]'],
-};
